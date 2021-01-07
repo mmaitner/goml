b52b5fed0c07b6ed36d7dd88ba04d5241b075ea2
ashtyn3 <ashtyn@gitmono.com>
2021-01-07 16:38:45 -0500

    got rid of fluff

--- cmd/index.js
+++
@@ -1,8 +0,0 @@
-let text_gbaiC = () => { const gbaiC = document.createElement('p');gbaiC.innerHTML = 'header is here!!'; return gbaiC}; let header_MRAjW = () => {const MRAjW = document.createElement('h1');  MRAjW.appendChild(text_gbaiC());  return MRAjW}; let text_XVlBz = () => { const XVlBz = document.createElement('p');XVlBz.innerHTML = 'hi'; return XVlBz}; let text_whTHc = () => { const whTHc = document.createElement('p');whTHc.innerHTML = 'hi'; return whTHc}; let button_tcuAx = () => { const tcuAx = document.createElement('button');  tcuAx.appendChild(text_whTHc()) ; return tcuAx};
-let body_body = () => { const body = document.createElement('div');
-
-body.appendChild(header_MRAjW())
-body.appendChild(text_XVlBz())
-body.appendChild(button_tcuAx())
-
-return body}
