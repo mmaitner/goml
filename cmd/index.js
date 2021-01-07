5d123defc1848cb2d410515b7691304ae65c23a7
ashtyn3 <ashtyn@gitmono.com>
2021-01-07 16:27:58 -0500

    made reusing components possible

--- cmd/index.js
+++ cmd/index.js
@@ -1,23 +1,8 @@
-let text_gbaiC = () => { const gbaiC = document.createElement('p');gbaiC.innerHTML = 'header is here!!'
-return gbaiC}
-
-let header_MRAjW = () => {const MRAjW = document.createElement('h1');
-
-MRAjW.appendChild(text_gbaiC())
-
-return MRAjW}
-let text_XVlBz = () => { const XVlBz = document.createElement('p');XVlBz.innerHTML = 'hi'
-return XVlBz}
-
-const button_whTHc= () => { const whTHc = document.createElement('button');
-
-whTHc.innerText = 'hi';;
-return whTHc}
-
+let text_gbaiC = () => { const gbaiC = document.createElement('p');gbaiC.innerHTML = 'header is here!!'; return gbaiC}; let header_MRAjW = () => {const MRAjW = document.createElement('h1');  MRAjW.appendChild(text_gbaiC());  return MRAjW}; let text_XVlBz = () => { const XVlBz = document.createElement('p');XVlBz.innerHTML = 'hi'; return XVlBz}; let text_whTHc = () => { const whTHc = document.createElement('p');whTHc.innerHTML = 'hi'; return whTHc}; let button_tcuAx = () => { const tcuAx = document.createElement('button');  tcuAx.appendChild(text_whTHc()) ; return tcuAx};
 let body_body = () => { const body = document.createElement('div');
 
 body.appendChild(header_MRAjW())
 body.appendChild(text_XVlBz())
-body.appendChild(button_whTHc())
+body.appendChild(button_tcuAx())
 
 return body}
--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ func main() {
 	p := goml.Text(goml.Tags{}, "hi")
 	h1 := goml.Header(goml.Tags{}, goml.Text(goml.Tags{}, "header is here!!"))
 
-	button := goml.Button(goml.Tags{}, "hi")
+	button := goml.Button(goml.Tags{}, goml.Text(goml.Tags{}, "hi"))
 
 	options := goml.RenderParams{}
 	render := goml.Render([]goml.Element{h1, p, button}, options)
